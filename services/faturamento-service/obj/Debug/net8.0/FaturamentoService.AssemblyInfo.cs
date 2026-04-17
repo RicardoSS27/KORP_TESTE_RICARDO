3aa8879568a65a441f5fdd4e4645c32b76388e13
Ricardo Sousa <71410420+RicardoSS27@users.noreply.github.com>
2026-04-17 16:20:38 -0300

    SALDO

--- services/faturamento-service/obj/Debug/net8.0/FaturamentoService.AssemblyInfo.cs
+++ services/faturamento-service/obj/Debug/net8.0/FaturamentoService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaturamentoService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9756ff591ad5bd2339f7a2d60edb323767724272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99223df1537d988358bf18c5cc1b4410ec564f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaturamentoService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaturamentoService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
